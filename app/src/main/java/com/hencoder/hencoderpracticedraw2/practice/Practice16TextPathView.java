3f3b2b276d2e92dc854bd65e93c3d58d3fb6a880
黄建洪 <2372635607@qq.com>
2018-01-18 15:21:18 +0800

    [文字路径] 获取文字的实际路径轮廓; paint.getTextPath();

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice16TextPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice16TextPathView.java
@@ -30,6 +30,7 @@ public class Practice16TextPathView extends View {
         paint.setTextSize(120);
 
         // 使用 Paint.getTextPath() 来获取文字的 Path
+        paint.getTextPath(text, 0,text.length(),0,0, textPath); //x 和 y 坐标轴的位置
 
         pathPaint.setStyle(Paint.Style.STROKE);
     }
@@ -40,6 +41,8 @@ public class Practice16TextPathView extends View {
 
         canvas.drawText(text, 50, 200, paint);
 
+        canvas.translate(50, 500);
         canvas.drawPath(textPath, pathPaint);
+
     }
 }
